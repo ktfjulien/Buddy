7d9f2e714ac24f3b7ed559cfca0122963543cc02
ktfjulien <ktfjulien@gmail.com>
2017-05-10 09:35:36 -0700

    stuff

--- Classes/UniversalStuff.cs
+++ Classes/UniversalStuff.cs
@@ -8,6 +8,6 @@ namespace Buddy {
     static class us {
         public static UserContext db = new UserContext();
         public static User currentuser;
-        public static int testinggithub;
+        //public static int testinggithub;
     }
 }
