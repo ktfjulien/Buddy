727dce8286f1d9f38136496ff6beedf3e86956e2
Julien Boker <ktfjulien@gmail.com>
2017-09-29 10:45:18 -0700

    sneeze

--- Forms/ConversationsForm.cs
+++ Forms/ConversationsForm.cs
@@ -12,20 +12,25 @@ namespace Buddy.Forms {
     public partial class ConversationsForm : Form {
         public ConversationsForm() {
             InitializeComponent();
-            listBox1.DataSource = us.currentuser.friends;
+            listBox1.DataSource = us.currentuser.friends.ToList();
         }
 
         private void ConversationsForm_Load(object sender, EventArgs e) {
             //foreach (var x in us.currentuser.friends) {
             //    listBox1.Items.Add(x.username);
             //}
-            var temp = us.currentuser.ReceivedMessages.Select(b => b);
-            foreach (var x in temp) {
-                if (x.sender.username == (string)listBox1.SelectedItem) {
-                    string.Join(Environment.NewLine, x.content);
+            try {
+                var temp = us.currentuser.ReceivedMessages.Select(b => b);
+                foreach (var x in temp) {
+                    if (x.sender.username == (string)listBox1.SelectedItem) {
+                        string.Join(Environment.NewLine, x.content);
+                    }
                 }
+                listBox1.SelectedIndex = 0;
+            } catch {
+
             }
-            listBox1.SelectedIndex = 0;
+            
         }
 
         private void LoadMessages(string otheruser) {
@@ -34,18 +39,18 @@ namespace Buddy.Forms {
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
             MessagesBox.Text = String.Empty;
-            User temp = us.db.Users.Find(listBox1.SelectedItem);
+            User temp = us.db.Users.Find(listBox1.SelectedItem.ToString());
             var z = new List<Message>();
 
-            //foreach (var x in temp.SentMessages) {
-            //    x.content = x.content.Insert(0, temp.username + ": ");
-            //    if (x.recipient == us.currentuser) {
-            //        z.Add(x);
-            //    }
-            //}
+            foreach (var x in temp.SentMessages) {
+                //x.content = x.content.Insert(0, temp.username + ": ");
+                if (x.recipient == us.currentuser) {
+                    z.Add(x);
+                }
+            }
 
             foreach (var x in us.currentuser.SentMessages) {
-                x.content = x.content.Insert(0, us.currentuser.username + ": ");
+                //x.content = x.content.Insert(0, us.currentuser.username + ": ");
                 if (x.recipient == temp) {
                     z.Add(x);
                 }
@@ -54,17 +59,13 @@ namespace Buddy.Forms {
             z.Sort((x, y) => x.TimeSent.CompareTo(y.TimeSent));
 
             foreach (var x in z) {
+                //if (x.sender.username == us.currentuser.username) {
+                //    MessagesBox.SelectionStart = 0;
+                //    MessagesBox.SelectionLength = us.currentuser.username.Length + 1;
+                //    MessagesBox.SelectionFont = new Font(MessagesBox.Font, System.Drawing.FontStyle.Bold);
+                //}
                 MessagesBox.Text += x.content + '\n';
             }
-
-            // Replacing this with new, conversation like sorted messages code
-
-            //var temp = us.currentuser.ReceivedMessages.ToArray();
-            //foreach (var x in temp) {
-            //    if (x.sender.username == (string)listBox1.SelectedItem) {
-            //        MessagesBox.Text += string.Join(Environment.NewLine, x.content);
-            //    }
-            //}
         }
 
         private void textBox1_Enter(object sender, EventArgs e) {
@@ -88,10 +89,20 @@ namespace Buddy.Forms {
         }
 
         private void AddFriendButton_Click(object sender, EventArgs e) {
-            this.Hide();
+            //this.Hide();
             var add = new Add_A_Friend_Screen();
             add.FormClosed += (s, args) => this.Show();
-            add.Show();
+            add.StartPosition = FormStartPosition.CenterParent;
+            add.ShowDialog();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            //listBox1.Items.Clear();
+            //foreach (var str in us.currentuser.friends) {
+            //    if (str.username.StartsWith(textBox1.Text, StringComparison.CurrentCultureIgnoreCase)) {
+            //        listBox1.Items.Add(str.username);
+            //    }
+            //}
         }
     }
 }
